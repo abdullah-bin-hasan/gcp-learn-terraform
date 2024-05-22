2c18836826808298fedbeaece9e1adbb1d86b3a3
Abdullah <abdullah.binhasan@outlook.com>
2024-05-22 21:59:17 +0600

    terraform module from local file

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "qwiklabs-gcp-02-b15184a54a09"
+
+}
+
+variable "name" {
+  description = "Name of the buckets to create."
+  type        = string
+  default     = "qwiklabs-gcp-02-b15184a54a09"
+}
